968c2845325668f33f092a8f1537a92804c5cd4d
Imed BEN YOUSSEF <imed.benyoussef@gmail.com>
2024-06-06 01:00:54 +0200

    fix test

--- src/aiglusoft.iam.client/src/app/shared/directive/toggle-password-visibility/toggle-password-visibility.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TogglePasswordVisibilityDirective } from './toggle-password-visibility.directive';
-
-describe('TogglePasswordVisibilityDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TogglePasswordVisibilityDirective();
-    expect(directive).toBeTruthy();
-  });
-});
