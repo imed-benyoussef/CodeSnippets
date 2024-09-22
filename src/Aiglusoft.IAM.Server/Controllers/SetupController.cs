b0df9284b27261b349c84c47d9765a9209584121
Imed BEN YOUSSEF <imed.benyoussef@gmail.com>
2024-09-22 11:40:17 +0200

    add endpoint /setup

---
+++ src/Aiglusoft.IAM.Server/Controllers/SetupController.cs
@@ -0,0 +1,35 @@
+﻿using Aiglusoft.IAM.Infrastructure.Persistence.DbContexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aiglusoft.IAM.Server.Controllers
+{
+  [ApiController]
+  [Route("setup")]
+  public class SetupController : ControllerBase
+  {
+    private readonly AppDbContext _context;
+
+    public SetupController(AppDbContext context)
+    {
+      _context = context;
+    }
+
+    // Endpoint pour exécuter les migrations de la base de données
+    [HttpPost("")]
+    public async Task<IActionResult> Setup()
+    {
+      try
+      {
+        // Appliquer les migrations à la base de données
+        await _context.Database.MigrateAsync();
+        return Ok("Migrations exécutées avec succès.");
+      }
+      catch (Exception ex)
+      {
+        // Retourner une erreur si les migrations échouent
+        return StatusCode(500, $"Erreur lors de l'exécution des migrations : {ex.Message}");
+      }
+    }
+  }
+}
